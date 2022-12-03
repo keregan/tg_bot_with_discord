6dbea0c56230be726bd309399d47acca7fb75d89
AlexPuchenkov <alex12121414@mail.ru>
2022-12-03 11:27:02 +0300

    connect api

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("bot")
+import apy_bot
+
+def main():
+    print(apy_bot.telebot_api)
+
+
+# if __name__ == '__main__':
+#     main()
+#     bot.polling(none_stop=True)
+
+# import requests
+# import time
+# import json
+# import telebot
+# import config
+# import bot_api
+#
+# headers = {"user agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chro"
+#                          "me/102.0.0.0 Safari/537.36"}
+# url = "https://yobit.net/api/3/info"
+# API = bot_api.api_bot
+# bot = telebot.TeleBot(API)
+#
+#
+# def main():
+#     print("Bot connect")
+#
+#
+# if __name__ == '__main__':
+#     main()
+#     bot.polling(none_stop=True)
