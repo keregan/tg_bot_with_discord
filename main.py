04d51ec193252bdbe1cfe573e2cf018f606d4c29
AlexPuchenkov <alex12121414@mail.ru>
2022-12-03 11:53:15 +0300

    First test bot

--- main.py
+++ main.py
@@ -1,48 +1,30 @@
-print("a")
-# import apy_bot
-# from aiogram import Bot, Dispatcher, executor, types
-#
-#
-# def main():
-#     bot_tg = Bot(token=apy_bot.telebot_api)
-#     dp_tg = Dispatcher(bot_tg)
-#
-#     async def send_welcome(message: types.Message):
-#         await message.reply(
-#             "Привет!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
-#
-#     async def echo(message: types.Message):
-#         await message.answer(message.text)
-
-    # print(apy_bot.telebot_api)
-
-# if __name__ == '__main__':
-#    executor.start_polling(dp_tg, skip_updates=True)
-
-
-
-# if __name__ == '__main__':
-#     main()
-#     bot.polling(none_stop=True)
-
 # import requests
 # import time
 # import json
 # import telebot
 # import config
-# import bot_api
-#
+import apy_bot
+
+from aiogram import Bot, Dispatcher, executor, types
+
 # headers = {"user agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chro"
 #                          "me/102.0.0.0 Safari/537.36"}
 # url = "https://yobit.net/api/3/info"
-# API = bot_api.api_bot
-# bot = telebot.TeleBot(API)
-#
-#
-# def main():
-#     print("Bot connect")
-#
-#
-# if __name__ == '__main__':
-#     main()
-#     bot.polling(none_stop=True)
+
+bot = Bot(token=apy_bot.telebot_api)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
+
