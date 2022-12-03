918d06494edc4b34a40ab15cdd3ee5c0cf9669ae
AlexPuchenkov <alex12121414@mail.ru>
2022-12-03 14:19:47 +0300

    Connect telegram_bot with discord_bot

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
-# import requests
-# import time
-# import json
-# import telebot
-# import config
+import requests
+import time
+import json
+import telebot
+import asyncio
+import discord
+import requests
 import apy_bot
+import random
+import threading
 
+from discord.ext import commands
 from aiogram import Bot, Dispatcher, executor, types
+from concurrent.futures import ThreadPoolExecutor
 
 # headers = {"user agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chro"
 #                          "me/102.0.0.0 Safari/537.36"}
 # url = "https://yobit.net/api/3/info"
 
-bot = Bot(token=apy_bot.telebot_api)
-dp = Dispatcher(bot)
+bot_telegramm = Bot(token=apy_bot.telebot_api)
+dp_telegramm = Dispatcher(bot_telegramm)
 
-@dp.message_handler(commands=['start'])
+
+bot_discord = commands.Bot(intents=discord.Intents.default(), command_prefix='><')
+client_discord = discord.Client(intents=discord.Intents.default())
+
+@bot_discord.event
+async def on_ready():
+    while True:
+        await tester()
+        random_poss = random.randint(55, 180)
+        random_poss = 1000
+        await asyncio.sleep(random_poss)
+
+@bot_discord.event
+async def tester():
+    channel = bot_discord.get_channel(982160360897396736)
+    await channel.send("test")
+
+
+@dp_telegramm.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
 
 
-@dp.message_handler()
+@dp_telegramm.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    text = message.text
+    await message.answer(text)
+
+
+
+def main():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(bot_discord.start(apy_bot.discord_api))
+    print("Discord_bot activated")
+    print("Telegramm_bot activated")
+    executor.start_polling(dp_telegramm)
+    loop.run_forever()
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
-
-
+    main()
