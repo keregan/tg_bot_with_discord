ce3d4682058a69da17493fcc5529df360174ec42
AlexPuchenkov <alex12121414@mail.ru>
2022-12-26 12:13:41 +0300

    Author message and time message

--- main.py
+++ main.py
@@ -13,22 +13,19 @@ from discord.ext import commands
 from aiogram import Bot, Dispatcher, executor, types
 from concurrent.futures import ThreadPoolExecutor
 
-# headers = {"user agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chro"
-#                          "me/102.0.0.0 Safari/537.36"}
-# url = "https://yobit.net/api/3/info"
+last_message = ({"last_mess"})
 
 # headers = {"user agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chro"
 #                          "me/102.0.0.0 Safari/537.36"}
 # url = "https://yobit.net/api/3/info"
+
 
 bot_telegramm = Bot(token=apy_bot.telebot_api)
 dp_telegramm = Dispatcher(bot_telegramm)
 
-
 bot_discord = commands.Bot(intents=discord.Intents.default(), command_prefix='><')
 client_discord = discord.Client(intents=discord.Intents.default())
 
-v_s_old = "2"
 
 def telegramm_send_all(text):
     params = {
@@ -36,6 +33,7 @@ def telegramm_send_all(text):
         'text': text,
     }
     response = requests.get('https://api.telegram.org/bot' + apy_bot.telebot_api + '/sendMessage', params=params)
+
 
 @bot_discord.event
 async def on_ready():
@@ -47,25 +45,38 @@ async def on_ready():
         random_poss = 3
         await asyncio.sleep(random_poss)
 
+
 @bot_discord.event
 async def tester():
     channel = bot_discord.get_channel(982160360897396736)
     await channel.send("test")
 
+
 @bot_discord.event
 async def last_message():
-    global  v_s
-    global v_s_old
+    global  text_message
     channel = bot_discord.get_channel(982160360897396736)
     messages = [message async for message in channel.history(limit=1)]
-    for msg in messages:
-        v_s = msg.content
-    # print(v_s)
-    # print(v_s_old)
-    if v_s != v_s_old:
-        telegramm_send_all(msg.content)
-        v_s_old = v_s
 
+    for msg in messages:
+        # text_author = msg.author
+        text_message = str(msg.author)[:-5] + " " + str(msg.created_at.strftime('%Y-%m-%d %H:%M:%S')) + " " + str(msg.content)
+        # text_datatime = msg.created_at
+        # text_jet = str(msg.author)[:-5] + " " + str(msg.created_at.strftime('%Y-%m-%d %H:%M:%S')) + " " + str(msg.content)
+        # print(text_jet)
+
+    try:
+        with open('last_message.json', encoding='utf-8') as file_json:
+            last_message = json.load(file_json)
+
+        if text_message != last_message["last_mess"]:
+            telegramm_send_all(text_message)
+            print(text_message)
+            last_message["last_mess"] = text_message
+            with open("last_message.json", "w", encoding='utf-8') as file_json:
+                json.dump(last_message, file_json, indent=4, ensure_ascii=False)
+    except:
+        pass
 
 
 @dp_telegramm.message_handler(commands=['start'])
